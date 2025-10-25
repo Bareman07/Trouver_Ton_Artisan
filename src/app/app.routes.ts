fdaf90e0dd62e1910f2ee44501efac9dc24e23cd
Bareman07 <146845508+Bareman07@users.noreply.github.com>
2025-10-25 03:51:08 +0200

    PageLegaleAjoute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { AccueilComponent } from './pages/accueil/accueil.component';
 import { AProposComponent } from './pages/a-propos/a-propos.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RechercheComponent } from './pages/recherche/recherche.component';
+import { MentionsLegalesComponent } from './pages/mentions-legales/mentions-legales.component';
+import { CookiesComponent } from './pages/cookies/cookies.component';
+import { AccessibiliteComponent } from './pages/accessibilite/accessibilite.component';
+import { DonneesPersonnellesComponent } from './pages/donnees-personnelles/donnees-personnelles.component';
 // Import the components pour les routes
 
 export const routes: Routes = [
@@ -10,6 +14,10 @@ export const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'a-propos', component: AProposComponent },
   { path: 'recherche', component: RechercheComponent },
+  { path: 'mentions-legales', component: MentionsLegalesComponent },
+  { path: 'cookies', component: CookiesComponent },
+  { path: 'accessibilite', component: AccessibiliteComponent },
+  { path: 'donnees-personnelles', component: DonneesPersonnellesComponent },
   { path: 'artisan/:id', loadComponent: () => import('./pages/artisan/artisan.component').then(m => m.ArtisanComponent) },
   { path: 'contact/:id', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
   // Route 404 (toujours en dernier)
